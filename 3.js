b8523a73edc492ceafad996a8cdb6bfda8f96bb2
Folarin Deb <103595605+folarindeb@users.noreply.github.com>
2022-05-10 09:55:28 -0400

    3

---
+++ 3.js
@@ -0,0 +1,4 @@
+//We need a function that can transform a number into a string. What ways of achieving this do you know?
+function numberToString(num) {
+    // Return a string of the number here!
+   if (num => 0 || num < 0) return num.toString();
